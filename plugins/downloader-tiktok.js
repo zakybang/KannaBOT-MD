e008e37ab3f78a8bd3993780f42324288f065ebd
lolimd <107601652+lolimd@users.noreply.github.com>
2022-06-20 15:34:36 +0700

    Add files via upload

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,9 +1,15 @@
-import xa from 'xfarr-api'
-let handler = async(m, { conn, usedPrefix, args, command }) => {
-    if(!args[0]) throw `Harap masukkan URL sebagai parameter!\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSdNmfmeJ/`
-    xa.Tiktok(args[0])
-    .then(async data => {   
-    let caption = `                   *「 T I K T O K 」*
+import { tiktokdl } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `*TIKTOK DOWNLOADER*
+*Nickname:* ${nickname}
+*Description:* ${description}
+
+_©Zakybot-Md_
                  ████████▀▀▀████
                  ████████────▀██
                  ████████──█▄──█
@@ -12,15 +18,11 @@ let handler = async(m, { conn, usedPrefix, args, command }) => {
                  █──█████──█████
                  █▄──▀▀▀──▄█████
                  ███▄▄▄▄▄███████
-──────────────────────────
-*📛 Title:* ${data.title}
-*🖼️ Quality:* ${data.medias[1].quality}
-*📊 Size:* ${data.medias[1].forrmattedSize}`
-    await conn.sendHydrated(m.chat, caption, global.wm, data.medias[1].url, `${args[0]}`, '🌎 L I N K', null, null, [[null, null],[null,null],[null,null]], m) 
-    })
+             di tiktok download sudah gw perbaiki
+`.trim(), m)
 }
-handler.command = /^(tiktok|tk|tkdl|td)$/i
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.help = ['tiktok <url>']
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 
 export default handler
